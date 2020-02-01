917948afc29d9fe5f13516762d798bfdde10fd97
genceyalcin <genceyalcin@vt.edu>
2020-02-01 07:23:38 -0500

    Oh boy

--- app/static/JS/live_comm.js
+++ app/static/JS/live_comm.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
 
-	var source = new EventSource('/refresh');
+	var source = new EventSource("/refresh");
 
 	source.onmessage = function(event) {
+		console.log("Message Received");
 		getFileExplorerContent();
 	}
 
