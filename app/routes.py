22a38dfcb78c0effcc5c0764de3ac7f53563b5ba
Thomas Szydlowski <thomas447@vt.edu>
2020-02-01 01:10:56 +0000

    initial sessions test

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,23 @@
 from app import app
 
+#from flask import session
+from flask import Flask, session, redirect, url_for, request, render_template
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+	if not session.get('logged_in'):
+		return redirect(url_for('login'))
+	return "Hello, World!"
+
+
+@app.route('/login')
+def login():
+	session['logged_in'] = True
+	return "Login"
+
+
+@app.route('/logout')
+def logout():
+	session['logged_in'] = False
+	return "Logged Out"
